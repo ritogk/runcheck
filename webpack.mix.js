88acbf3779ad72e62b96a37500c37084e503102b
root <homing-job@yahoo.co.jp>
2022-04-18 14:03:28 +0900

    aa

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-const mix = require('laravel-mix');
+const mix = require("laravel-mix")
 
 /*
  |--------------------------------------------------------------------------
@@ -11,20 +11,31 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix
+  .js("resources/js/app.js", "public/js")
+  .sass("resources/sass/app.scss", "public/css")
 
-mix.styles([
-    //'resources/sass/style.css',
-    'resources/sass/select2.min.css',
-//    'resources/css/fontawesome-all.min.css',
-], 'public/css/all.css').version();
+mix
+  .styles(
+    [
+      //'resources/sass/style.css',
+      "resources/sass/select2.min.css",
+      //    'resources/css/fontawesome-all.min.css',
+    ],
+    "public/css/all.css"
+  )
+  .version()
 
-mix.scripts([
-    //'resources/js/scriptYoutube.js',
-    //'resources/js/scriptSelVideo.js',
-    'resources/js/smartScaling.js',
-    'resources/js/select2.min.js',
-    'resources/js/top_script.js',
-    //'resources/js/jquery-3.4.1.min.js',
-], 'public/js/all.js').version();
+mix
+  .scripts(
+    [
+      //'resources/js/scriptYoutube.js',
+      //'resources/js/scriptSelVideo.js',
+      //'resources/js/smartScaling.js',
+      "resources/js/select2.min.js",
+      //"resources/js/top_script.js",
+      //'resources/js/jquery-3.4.1.min.js',
+    ],
+    "public/js/all.js"
+  )
+  .version()
