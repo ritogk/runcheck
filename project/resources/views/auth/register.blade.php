e9b4362972c3a1a94c26e11cbd686e46c23502ca
root <homing-job@yahoo.co.jp>
2022-04-19 15:32:11 +0900

    padding調整

--- project/resources/views/auth/register.blade.php
+++ project/resources/views/auth/register.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container py-3">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('登録') }}</div>
+                <div class="card-header">{{ __('新規登録') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
