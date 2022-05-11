087735e8871dbd89c1590a9cfebb390b4612d229
root <homing-job@yahoo.co.jp>
2022-05-11 13:22:52 +0900

    navbarにこのアプリについて 追加

--- project/resources/views/layouts/app.blade.php
+++ project/resources/views/layouts/app.blade.php
@@ -73,6 +73,9 @@
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('lp.ja') }}">このアプリについて</a>
+                        </li>
                         <!-- Authentication Links -->
                         @guest
                             <li class="nav-item">
