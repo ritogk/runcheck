021cfac6429714dacf46ead70963543b20aaf142
ryoji <ryoji@unko.com>
2023-04-17 14:43:52 +0900

    IoeratuibKigSrvuseさｊｙせいう

--- project/app/Services/OperationLogService.php
+++ project/app/Services/OperationLogService.php
@@ -2,9 +2,21 @@
 
 namespace App\Services;
 
+use App\Model\OperationLog;
+
 class OperationLogService
 {
-  public function send(): void
+  /**
+   * 操作ログ更新
+   *
+   * @param integer $operation_cd
+   * @return void
+   */
+  public function update_operation_log(int $operation_cd): void
   {
+    $operation_log = OperationLog::where('operation_cd', $operation_cd)->first();
+    $operation_log->execution_cnt = $operation_log->execution_cnt + 1;
+    $operation_log->save();
+    return;
   }
 }
