bff4e478c25bcab93e7525ae5566cb6195688a6a
ryoji <ryoji@unko.com>
2023-04-17 14:10:46 +0900

    認証周りの処理作成

--- project/app/Services/AuthenticationService.php
+++ project/app/Services/AuthenticationService.php
@@ -2,17 +2,48 @@
 
 namespace App\Services;
 
+use Illuminate\Support\Facades\Auth;
+use App\Model\User;
+
 class AuthenticationService
 {
-  public function login(): void
+
+  /**
+   * ログイン
+   *
+   * @param string $email
+   * @param string $password
+   * @param boolean $remeber
+   * @return User|null
+   */
+  public function login(string $email, string $password, bool $remeber): ?User
   {
+    $credentials = ['email' => $email, 'password' => $password];
+    if (Auth::guard()->attempt($credentials, $remeber)) {
+      $user = Auth::user();
+      session()->regenerate(true);
+      return $user;
+    }
+    return null;
   }
 
+  /**
+   * ログアウト
+   *
+   * @return void
+   */
   public function logout(): void
   {
+    Auth::guard()->logout();
   }
 
-  public function me(): void
+  /**
+   * ユーザー情報取得
+   *
+   * @return User|null
+   */
+  public function me(): ?User
   {
+    return Auth::user();
   }
 }
