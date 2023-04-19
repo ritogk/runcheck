d5df0193908b18ff071e333930c26472c9cb25f0
ryoji <ryoji@unko.com>
2023-04-19 14:29:19 +0900

    create session service

---
+++ project/app/Services/SessionStorage.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Services;
+
+class SessionStorage
+{
+  // セッションのキー
+  const KEY_YOUTUBE_ACCESS_TOKEN = 'YOUTUBE_ACCESS_TOKEN';
+
+  /**
+   * セッションに値をセットする
+   *
+   * @param string $key
+   * @param string $value
+   * @return void
+   */
+  public function put(string $key, string $value)
+  {
+    session()->put($key, $value);
+  }
+
+  /**
+   * セッションから値を取得する
+   * セッションキーが存在しない場合はnullが変える
+   *
+   * @param string $key
+   * @return mixed
+   */
+  public function get(string $key)
+  {
+    return session()->get($key);
+  }
+}
