8efe3b3b399ecfffe05faf37832419217f0b5ec0
ryoji <ryoji@unko.com>
2023-04-20 15:39:27 +0900

    もれ

--- project/database/migrations/2023_04_17_105845_create_youtube_tokens.php
+++ project/database/migrations/2023_04_17_105845_create_youtube_tokens.php
@@ -18,6 +18,7 @@ class CreateYoutubeTokens extends Migration
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
             $table->string('refresh_token');
+            $table->softDeletes()->index();
             $table->timestamps();
         });
     }
