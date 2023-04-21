41879fe32134ca77a80af96b18e72b5f260d7616
ryoji <ryoji@unko.com>
2023-04-21 10:49:29 +0900

    ログイン毎のトークン読み出し処理追加

--- project/app/UseCase/Authentication/LoginAction.php
+++ project/app/UseCase/Authentication/LoginAction.php
@@ -4,9 +4,22 @@ namespace App\UseCase\Authentication;
 
 use Illuminate\Support\Facades\Auth;
 use App\Model\User;
+// core
+use App\Core\YouTube\OAuthYoutubeClient;
+
+// usecase
+use App\UseCase\SessionStorageAction;
 
 class LoginAction
 {
+  private SessionStorageAction $session_action;
+  private OAuthYoutubeClient $client;
+  public function __construct(SessionStorageAction $session_action, OAuthYoutubeClient $client)
+  {
+    $this->session_action = $session_action;
+    $this->client = $client;
+  }
+
   /**
    * Undocumented function
    *
@@ -18,11 +31,17 @@ class LoginAction
   public function login(string $email, string $password, bool $remeber): ?User
   {
     $credentials = ['email' => $email, 'password' => $password];
-    if (Auth::guard()->attempt($credentials, $remeber)) {
-      $user = Auth::guard()->user();
-      session()->regenerate(true);
-      return $user;
+    if (!Auth::guard()->attempt($credentials, $remeber)) return null;
+
+    $user = Auth::guard()->user();
+    session()->regenerate(true);
+    $youtube_token = $user->youtube_token;
+    if ($youtube_token) {
+      // リフレッシュトークンからアクセストークンを生成
+      $token = $this->client->generate_token($youtube_token->refresh_token);
+      unset($token['refresh_token']);
+      $this->session_action->put(SessionStorageAction::KEY_YOUTUBE_ACCESS_TOKEN, $token);
     }
-    return null;
+    return $user;
   }
 }
