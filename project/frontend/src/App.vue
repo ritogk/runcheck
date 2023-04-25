03100d6a0492f384b5166f6d4c22d14ed5afb0f6
ryoji <ryoji@unko.com>
2023-04-25 10:47:22 +0900

    ts化

--- project/frontend/src/App.vue
+++ project/frontend/src/App.vue
@@ -2,13 +2,15 @@
   <Dashboard />
 </template>
 
-<script>
+<script lang="ts">
+import { Options, Vue } from "vue-class-component"
 import Dashboard from "./components/Dashboard.vue"
 import "./tailwind.css"
-export default {
-  name: "App",
+
+@Options({
   components: {
     Dashboard,
   },
-}
+})
+export default class App extends Vue {}
 </script>
