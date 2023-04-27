2e82dfd152cb8f47c33482f5311092548d7a2201
ryoji <ryoji@unko.com>
2023-04-27 12:16:28 +0900

    状態をコンポーネント間で共有できるようにした

--- project/frontend/src/App.vue
+++ project/frontend/src/App.vue
@@ -1,6 +1,11 @@
 <script setup lang="ts">
 import Dashboard from "./pages/Dashboard.vue"
 import "./tailwind.css"
+import { provide } from "vue"
+import { useUserState, useUserStateKey } from "@/components/useUserState"
+
+const userState = useUserState()
+provide(useUserStateKey, userState)
 </script>
 
 <template>
