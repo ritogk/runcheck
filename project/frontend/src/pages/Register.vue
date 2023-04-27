4b213b192237ffe390a116f2946699f152fbfa10
ryoji <ryoji@unko.com>
2023-04-27 09:41:42 +0900

    パスワードの見た目調整

--- project/frontend/src/pages/Register.vue
+++ project/frontend/src/pages/Register.vue
@@ -16,6 +16,21 @@ const form = {
   passwordConfirm: ref<HTMLInputElement | null>(null),
 }
 
+const onSwitchPasswordVisibility = (type: "password" | "passwordConfirm") => {
+  switch (type) {
+    case "password":
+      if (form.password.value === null) return
+      form.password.value.type =
+        form.password.value.type === "password" ? "text" : "password"
+      break
+    case "passwordConfirm":
+      if (form.passwordConfirm.value === null) return
+      form.passwordConfirm.value.type =
+        form.passwordConfirm.value.type === "password" ? "text" : "password"
+      break
+  }
+}
+
 const onSubmit = async () => {
   if (
     form.hanndleName.value === null ||
@@ -158,42 +173,71 @@ const onSubmit = async () => {
               ></label
             >
             <div class="mt-2">
-              <input
-                id="password"
-                name="password"
-                type="password"
-                autocomplete="new-password"
-                :ref="form.password"
-                required
-                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
+              <div class="relative mt-2 rounded-md shadow-sm">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autocomplete="new-password"
+                  :ref="form.password"
+                  required
+                  pattern="^(?=.*\d)(?=.*[a-zA-Z]).{8,}$"
+                  title="半角英数字と半角数字を組み合わせて8文字以上で入力してください。"
+                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  placeholder="パスワード"
+                />
+                <div class="absolute inset-y-0 right-0 flex items-center pr-3">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="fill-gray-400 hover:fill-gray-300"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 200 145.522"
+                    @click="onSwitchPasswordVisibility('password')"
+                  >
+                    <path
+                      d="M99.363,0C52.631,0,14.243,30.17,0,72.083,14.243,114,52.631,145.522,99.363,145.522S185.754,114,200,72.083A106.312,106.312,0,0,0,99.363,0ZM97.382,24.478a47.647,47.647,0,1,1,1.981,95.254,47.643,47.643,0,1,1-1.981-95.265ZM99.363,39.1A32.987,32.987,0,1,0,132.35,72.083,32.987,32.987,0,0,0,99.363,39.1Zm8.233,8.743a11.454,11.454,0,1,1-11.215,11.69A11.455,11.455,0,0,1,107.6,47.839Z"
+                    />
+                  </svg>
+                </div>
+              </div>
             </div>
           </div>
 
           <div>
-            <label
-              for="password-confirmation"
-              class="block text-sm font-medium leading-6 text-gray-900"
-              >パスワード(確認)<span
-                class="text-red-400 text-lg leading-none"
-                title="このフィールドは必須です"
-                >*</span
-              ></label
-            >
             <div class="mt-2">
-              <input
-                id="password-confirmation"
-                name="password-confirmation"
-                type="password"
-                autocomplete="passwordConfirm"
-                :ref="form.passwordConfirm"
-                required
-                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
+              <div class="relative mt-2 rounded-md shadow-sm">
+                <input
+                  id="passwordConfirm"
+                  name="passwordConfirm"
+                  type="password"
+                  autocomplete="password-confirm"
+                  :ref="form.passwordConfirm"
+                  required
+                  pattern="^(?=.*\d)(?=.*[a-zA-Z]).{8,}$"
+                  title="半角英数字と半角数字を組み合わせて8文字以上で入力してください。"
+                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  placeholder="確認"
+                />
+                <div class="absolute inset-y-0 right-0 flex items-center pr-3">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="fill-gray-400 hover:fill-gray-300"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 200 145.522"
+                    @click="onSwitchPasswordVisibility('passwordConfirm')"
+                  >
+                    <path
+                      d="M99.363,0C52.631,0,14.243,30.17,0,72.083,14.243,114,52.631,145.522,99.363,145.522S185.754,114,200,72.083A106.312,106.312,0,0,0,99.363,0ZM97.382,24.478a47.647,47.647,0,1,1,1.981,95.254,47.643,47.643,0,1,1-1.981-95.265ZM99.363,39.1A32.987,32.987,0,1,0,132.35,72.083,32.987,32.987,0,0,0,99.363,39.1Zm8.233,8.743a11.454,11.454,0,1,1-11.215,11.69A11.455,11.455,0,0,1,107.6,47.839Z"
+                    />
+                  </svg>
+                </div>
+              </div>
             </div>
           </div>
 
-          <div>
+          <div class="mt-4">
             <button
               type="button"
               class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
