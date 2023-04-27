869686a59a0a38bc5703078510383882139e9bbb
ryoji <ryoji@unko.com>
2023-04-27 11:11:46 +0900

    ui微修正

--- project/frontend/src/pages/Register.vue
+++ project/frontend/src/pages/Register.vue
@@ -106,7 +106,7 @@ const onSubmit = async () => {
 
     <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
       <div class="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
-        <div>
+        <div class="space-y-4">
           <div>
             <label
               for="hundle-name"
@@ -118,7 +118,7 @@ const onSubmit = async () => {
                 >*</span
               ></label
             >
-            <div class="mt-1">
+            <div class="mt-2">
               <input
                 id="hundle-name"
                 name="hundle-name"
@@ -131,7 +131,7 @@ const onSubmit = async () => {
             </div>
           </div>
 
-          <div class="mt-2">
+          <div>
             <label
               for="car-type"
               class="block text-sm font-medium leading-6 text-gray-900"
@@ -141,7 +141,7 @@ const onSubmit = async () => {
                 >*</span
               ></label
             >
-            <div class="mt-1">
+            <div class="mt-2">
               <input
                 id="car-type"
                 name="car-type"
@@ -154,7 +154,7 @@ const onSubmit = async () => {
             </div>
           </div>
 
-          <div class="mt-2">
+          <div>
             <label
               for="email"
               class="block text-sm font-medium leading-6 text-gray-900"
@@ -164,7 +164,7 @@ const onSubmit = async () => {
                 >*</span
               ></label
             >
-            <div class="mt-1">
+            <div class="mt-2">
               <input
                 id="email"
                 name="email"
@@ -177,7 +177,7 @@ const onSubmit = async () => {
             </div>
           </div>
 
-          <div class="mt-2">
+          <div>
             <label
               for="password"
               class="block text-sm font-medium leading-6 text-gray-900"
@@ -187,69 +187,74 @@ const onSubmit = async () => {
                 >*</span
               ></label
             >
-            <div class="mt-1">
-              <div class="relative mt-2 rounded-md shadow-sm">
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  autocomplete="new-password"
-                  :ref="form.password"
-                  required
-                  pattern="^(?=.*\d)(?=.*[a-zA-Z]).{8,}$"
-                  title="半角英字と半角数字を組み合わせて8文字以上で入力してください。"
-                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  placeholder="パスワード"
-                />
-                <div class="absolute inset-y-0 right-0 flex items-center pr-3">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    class="fill-gray-400 hover:fill-gray-300"
-                    width="20"
-                    height="20"
-                    viewBox="0 0 200 145.522"
-                    @click="onSwitchPasswordVisibility('password')"
+            <div class="mt-2">
+              <div class="flex gap-2">
+                <div class="relative mt-2 rounded-md shadow-sm w-1/2">
+                  <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    autocomplete="new-password"
+                    :ref="form.password"
+                    required
+                    pattern="^(?=.*\d)(?=.*[a-zA-Z]).{8,}$"
+                    title="半角英字と半角数字を組み合わせて8文字以上で入力してください。"
+                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    placeholder="パスワード"
+                  />
+                  <div
+                    class="absolute inset-y-0 right-0 flex items-center pr-3"
                   >
-                    <path
-                      d="M99.363,0C52.631,0,14.243,30.17,0,72.083,14.243,114,52.631,145.522,99.363,145.522S185.754,114,200,72.083A106.312,106.312,0,0,0,99.363,0ZM97.382,24.478a47.647,47.647,0,1,1,1.981,95.254,47.643,47.643,0,1,1-1.981-95.265ZM99.363,39.1A32.987,32.987,0,1,0,132.35,72.083,32.987,32.987,0,0,0,99.363,39.1Zm8.233,8.743a11.454,11.454,0,1,1-11.215,11.69A11.455,11.455,0,0,1,107.6,47.839Z"
-                    />
-                  </svg>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="fill-gray-400 hover:fill-gray-300"
+                      width="20"
+                      height="20"
+                      viewBox="0 0 200 145.522"
+                      @click="onSwitchPasswordVisibility('password')"
+                    >
+                      <path
+                        d="M99.363,0C52.631,0,14.243,30.17,0,72.083,14.243,114,52.631,145.522,99.363,145.522S185.754,114,200,72.083A106.312,106.312,0,0,0,99.363,0ZM97.382,24.478a47.647,47.647,0,1,1,1.981,95.254,47.643,47.643,0,1,1-1.981-95.265ZM99.363,39.1A32.987,32.987,0,1,0,132.35,72.083,32.987,32.987,0,0,0,99.363,39.1Zm8.233,8.743a11.454,11.454,0,1,1-11.215,11.69A11.455,11.455,0,0,1,107.6,47.839Z"
+                      />
+                    </svg>
+                  </div>
+                </div>
+                <div class="relative mt-2 rounded-md shadow-sm w-1/2">
+                  <input
+                    id="passwordConfirm"
+                    name="passwordConfirm"
+                    type="password"
+                    autocomplete="password-confirm"
+                    :ref="form.passwordConfirm"
+                    required
+                    pattern="^(?=.*\d)(?=.*[a-zA-Z]).{8,}$"
+                    title="半角英字と半角数字を組み合わせて8文字以上で入力してください。"
+                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    placeholder="確認"
+                  />
+                  <div
+                    class="absolute inset-y-0 right-0 flex items-center pr-3"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="fill-gray-400 hover:fill-gray-300"
+                      width="20"
+                      height="20"
+                      viewBox="0 0 200 145.522"
+                      @click="onSwitchPasswordVisibility('passwordConfirm')"
+                    >
+                      <path
+                        d="M99.363,0C52.631,0,14.243,30.17,0,72.083,14.243,114,52.631,145.522,99.363,145.522S185.754,114,200,72.083A106.312,106.312,0,0,0,99.363,0ZM97.382,24.478a47.647,47.647,0,1,1,1.981,95.254,47.643,47.643,0,1,1-1.981-95.265ZM99.363,39.1A32.987,32.987,0,1,0,132.35,72.083,32.987,32.987,0,0,0,99.363,39.1Zm8.233,8.743a11.454,11.454,0,1,1-11.215,11.69A11.455,11.455,0,0,1,107.6,47.839Z"
+                      />
+                    </svg>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
 
           <div>
-            <div class="mt-2">
-              <div class="relative mt-2 rounded-md shadow-sm">
-                <input
-                  id="passwordConfirm"
-                  name="passwordConfirm"
-                  type="password"
-                  autocomplete="password-confirm"
-                  :ref="form.passwordConfirm"
-                  required
-                  pattern="^(?=.*\d)(?=.*[a-zA-Z]).{8,}$"
-                  title="半角英字と半角数字を組み合わせて8文字以上で入力してください。"
-                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  placeholder="確認"
-                />
-                <div class="absolute inset-y-0 right-0 flex items-center pr-3">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    class="fill-gray-400 hover:fill-gray-300"
-                    width="20"
-                    height="20"
-                    viewBox="0 0 200 145.522"
-                    @click="onSwitchPasswordVisibility('passwordConfirm')"
-                  >
-                    <path
-                      d="M99.363,0C52.631,0,14.243,30.17,0,72.083,14.243,114,52.631,145.522,99.363,145.522S185.754,114,200,72.083A106.312,106.312,0,0,0,99.363,0ZM97.382,24.478a47.647,47.647,0,1,1,1.981,95.254,47.643,47.643,0,1,1-1.981-95.265ZM99.363,39.1A32.987,32.987,0,1,0,132.35,72.083,32.987,32.987,0,0,0,99.363,39.1Zm8.233,8.743a11.454,11.454,0,1,1-11.215,11.69A11.455,11.455,0,0,1,107.6,47.839Z"
-                    />
-                  </svg>
-                </div>
-              </div>
-            </div>
+            <div></div>
           </div>
 
           <div class="mt-4">
