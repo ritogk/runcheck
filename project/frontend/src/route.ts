97ba3cd0eec81f834705db5cca33abc96fb96019
ryoji <ryoji@unko.com>
2023-04-27 16:33:25 +0900

    refactor

--- project/frontend/src/route.ts
+++ project/frontend/src/route.ts
@@ -1,8 +1,8 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router"
-import Main from "@/pages/Main.vue"
-import Register from "@/pages/Register.vue"
-import Login from "@/pages/Login.vue"
-import Home from "@/pages/Home.vue"
+import Main from "@/pages/main/Main.vue"
+import Register from "@/pages/register/Register.vue"
+import Login from "@/pages/login/Login.vue"
+import Home from "@/pages/home/Home.vue"
 
 const routes: Array<RouteRecordRaw> = [
   {
