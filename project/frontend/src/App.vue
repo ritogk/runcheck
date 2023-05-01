b8ba49b5000dbd3cb0eac2d59cfee8d9ea177629
ritogk <homing0321r4cfw@yahoo.co.jp>
2023-05-01 16:31:00 +0900

    タッチのズームを無効化

--- project/frontend/src/App.vue
+++ project/frontend/src/App.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
-import Dashboard from "@/pages/dashboard/Dashboard.vue"
-import "./tailwind.css"
-import { provide } from "vue"
-import { useUserState, useUserStateKey } from "@/components/useUserState"
+import Dashboard from "@/pages/dashboard/Dashboard.vue";
+import "./tailwind.css";
+import { provide } from "vue";
+import { useUserState, useUserStateKey } from "@/components/useUserState";
 
-const userState = useUserState()
-provide(useUserStateKey, userState)
-userState.load()
+const userState = useUserState();
+provide(useUserStateKey, userState);
+userState.load();
 </script>
 
 <template>
-  <Dashboard />
+  <Dashboard class="touch-manipulation" />
 </template>
--- project/resources/views/app.blade.php
+++ project/resources/views/app.blade.php
@@ -3,10 +3,11 @@
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no">
     <title>RunCheck</title>
     <script defer src="{{ asset('/spa/js/chunk-vendors.js?v='.time(), true) }}"></script>
     <script defer src="{{ asset('/spa/js/app.js?v='.time(), true) }}"></script>
+    
   </head>
   <body>
     <noscript>
