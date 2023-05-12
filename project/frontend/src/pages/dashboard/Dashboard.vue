9514d1db134ffa3d44ea2120d821027ef94274a9
ryoji <ryoji@unko.com>
2023-05-12 16:03:06 +0900

    デスクトップのサイドバーにホームボタンがなかったので追加

--- project/frontend/src/pages/dashboard/Dashboard.vue
+++ project/frontend/src/pages/dashboard/Dashboard.vue
@@ -237,6 +237,19 @@ const sidebarOpen = ref(false)
           <ul role="list" class="flex flex-1 flex-col gap-y-7">
             <li>
               <ul role="list" class="-mx-2 space-y-1">
+                <li v-if="userState.logined">
+                  <a
+                    class="text-gray-400 hover:text-white hover:bg-gray-800 group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold cursor-pointer"
+                    @click="hundleHomeClick()"
+                  >
+                    <component
+                      :is="UserIcon"
+                      class="h-6 w-6 shrink-0"
+                      aria-hidden="true"
+                    />
+                    {{ userState.user.name }}
+                  </a>
+                </li>
                 <li v-for="item in navigation" :key="item.name">
                   <a
                     v-if="item.show.value"
