25956535e64c0b6e41538ef544519b7a7a1c84eb
ritogk <homing0321r4cfw@yahoo.co.jp>
2023-05-13 12:35:40 +0900

    スキーマ作成

--- project/frontend/src/core/openapiClient/apis/index.ts
+++ project/frontend/src/core/openapiClient/apis/index.ts
@@ -3,5 +3,6 @@
 export * from './AuthenticationApi';
 export * from './ComparisonsApi';
 export * from './OperationLogApi';
+export * from './StatusApi';
 export * from './UsersApi';
 export * from './YoutubeApi';
