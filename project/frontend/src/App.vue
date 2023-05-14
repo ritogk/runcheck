5d5cd75de1f4603829e9033e17a5f1efb02e0143
ritogk <homing0321r4cfw@yahoo.co.jp>
2023-05-15 08:35:14 +0900

    loadingコンポーネント作成

--- project/frontend/src/App.vue
+++ project/frontend/src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import Dashboard from "@/app/Dashboard.vue"
+import Loading from "@/app/Loading.vue"
 import "./tailwind.css"
 </script>
 
 <template>
   <Dashboard class="touch-manipulation" />
+  <Loading></Loading>
 </template>
