ee800625a6ddb5bea6701506b3b640a116b9134c
ryoji <ryoji@unko.com>
2023-05-15 15:09:07 +0900

    スピナーを追加

--- project/frontend/src/app/pages/main/main-parts/YoutubeSelectorModal.vue
+++ project/frontend/src/app/pages/main/main-parts/YoutubeSelectorModal.vue
@@ -232,44 +232,80 @@ if (code) callbackYoutubeOauth(code)
                     id="options"
                     role="listbox"
                   >
-                    <!-- スケルトン -->
-                    <div v-if="!youtubeListState.subscription.read.value">
-                      <li
-                        v-for="i in 5"
-                        :key="i"
-                        class="animate-pulse group flex cursor-default select-none rounded-xl p-3 border-b border-b-gray-100 focus:bg-gray-100"
-                        id="option-1"
-                        role="option"
-                        tabindex="-1"
-                      >
-                        <div
-                          class="flex h-10 w-10 flex-none items-center justify-center rounded-lg bg-gray-200"
+                    <div class="relative">
+                      <!-- スケルトン -->
+                      <div v-if="!youtubeListState.subscription.read.value">
+                        <li
+                          v-for="i in 5"
+                          :key="i"
+                          class="animate-pulse group flex cursor-default select-none rounded-xl p-3 border-b border-b-gray-100 focus:bg-gray-100"
+                          id="option-1"
+                          role="option"
+                          tabindex="-1"
                         >
-                          <svg
-                            class="text-white w-[24px] h-[24px]"
-                            fill="none"
-                            stroke="currentColor"
-                            viewBox="0 0 24 24"
-                            xmlns="http://www.w3.org/2000/svg"
-                            aria-hidden="true"
+                          <div
+                            class="flex h-10 w-10 flex-none items-center justify-center rounded-lg bg-gray-200"
                           >
-                            <path
-                              stroke-linecap="round"
-                              d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z"
-                            ></path>
-                          </svg>
-                        </div>
-                        <div class="ml-4 flex-auto">
-                          <!-- Active: "text-gray-900", Not Active: "text-gray-700" -->
-                          <div
-                            class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-4/5 mb-4"
-                          ></div>
-                          <!-- Active: "text-gray-700", Not Active: "text-gray-500" -->
-                          <div
-                            class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-3/5"
-                          ></div>
-                        </div>
-                      </li>
+                            <svg
+                              class="text-white w-[24px] h-[24px]"
+                              fill="none"
+                              stroke="currentColor"
+                              viewBox="0 0 24 24"
+                              xmlns="http://www.w3.org/2000/svg"
+                              aria-hidden="true"
+                            >
+                              <path
+                                stroke-linecap="round"
+                                d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z"
+                              ></path>
+                            </svg>
+                          </div>
+                          <div class="ml-4 flex-auto">
+                            <!-- Active: "text-gray-900", Not Active: "text-gray-700" -->
+                            <div
+                              class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-4/5 mb-4"
+                            ></div>
+                            <!-- Active: "text-gray-700", Not Active: "text-gray-500" -->
+                            <div
+                              class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-3/5"
+                            ></div>
+                          </div>
+                        </li>
+                      </div>
+                      <!-- ローディングの背景 -->
+                      <div
+                        class="absolute top-0 left-0 w-full h-full bg-gray-50 opacity-10 rounded-lg backdrop-blur-lg brightness-50"
+                        v-if="
+                          youtubeListState.subscription.isReading.value &&
+                          filteredVideos.length === 0
+                        "
+                      ></div>
+                      <!-- ローディングのスピナー -->
+                      <div
+                        class="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2"
+                        v-if="
+                          youtubeListState.subscription.isReading.value &&
+                          filteredVideos.length === 0
+                        "
+                      >
+                        <svg
+                          aria-hidden="true"
+                          class="w-12 h-12 mr-2 text-gray-300 animate-spin fill-blue-500"
+                          viewBox="0 0 100 101"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                            fill="currentColor"
+                          />
+                          <path
+                            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                            fill="currentFill"
+                          />
+                        </svg>
+                        <span class="sr-only">Loading...</span>
+                      </div>
                     </div>
 
                     <!-- Active: "bg-gray-100" -->
