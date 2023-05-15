64ca2b27b0134a6e198b45ec992a80f24ea1963a
ryoji <ryoji@unko.com>
2023-05-15 14:27:14 +0900

    動画の調整要素のアコーディオン化

--- project/frontend/src/app/pages/main/main-parts/PlayerOneArea.vue
+++ project/frontend/src/app/pages/main/main-parts/PlayerOneArea.vue
@@ -69,187 +69,217 @@ const hundleVideoSeek = async (seconds: number) => {
   )
 }
 </script>
-<template>
-  <!-- ajust-->
-  <div v-show="!useMainState.syncPlayer.subscription.synced.value">
-    <!-- 進む -->
-    <div class="mt-2 flex gap-2 justify-between">
-      <button
-        class="rounded-md shadow-sm bg-white w-1/4 px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
-        @click="hundleVideoSeek(1)"
-      >
-        <div class="flex items-center justify-center">
-          <ChevronDoubleRightIcon
-            class="h-5 w-5 text-gray-400"
-            aria-hidden="true"
-          />
-          1s
-        </div>
-      </button>
-      <button
-        class="rounded-md shadow-sm bg-white w-1/4 px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
-        @click="hundleVideoSeek(0.5)"
-      >
-        <div class="flex items-center justify-center">
-          <ChevronDoubleRightIcon
-            class="h-5 w-5 text-gray-400"
-            aria-hidden="true"
-          />
-          0.5s
-        </div>
-      </button>
-      <button
-        class="rounded-md shadow-sm bg-white w-1/4 px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
-        @click="hundleVideoSeek(0.1)"
-      >
-        <div class="flex items-center justify-center">
-          <ChevronDoubleRightIcon
-            class="h-5 w-5 text-gray-400"
-            aria-hidden="true"
-          />
-          0.1s
-        </div>
-      </button>
-      <button
-        class="rounded-md shadow-sm bg-white w-1/4 px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
-        @click="hundleVideoSeek(0.05)"
-      >
-        <div class="flex items-center justify-center">
-          <ChevronDoubleRightIcon
-            class="h-5 w-5 text-gray-400"
-            aria-hidden="true"
-          />
-          0.05s
-        </div>
-      </button>
-    </div>
-    <!-- 戻る -->
-    <div class="mt-2 flex gap-2 justify-between">
-      <button
-        class="rounded-md shadow-sm bg-white w-1/4 px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
-        @click="hundleVideoSeek(-1)"
-      >
-        <div class="flex items-center justify-center">
-          <ChevronDoubleLeftIcon
-            class="h-5 w-5 text-gray-400"
-            aria-hidden="true"
-          />
-          1s
-        </div>
-      </button>
-      <button
-        class="rounded-md shadow-sm bg-white w-1/4 px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
-        @click="hundleVideoSeek(-0.5)"
-      >
-        <div class="flex items-center justify-center">
-          <ChevronDoubleLeftIcon
-            class="h-5 w-5 text-gray-400"
-            aria-hidden="true"
-          />
-          0.5s
-        </div>
-      </button>
-      <button
-        class="rounded-md shadow-sm bg-white w-1/4 px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
-        @click="hundleVideoSeek(-0.1)"
-      >
-        <div class="flex items-center justify-center">
-          <ChevronDoubleLeftIcon
-            class="h-5 w-5 text-gray-400"
-            aria-hidden="true"
-          />
-          0.1s
-        </div>
-      </button>
-      <button
-        class="rounded-md shadow-sm bg-white w-1/4 px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
-        @click="hundleVideoSeek(-0.05)"
-      >
-        <div class="flex items-center justify-center">
-          <ChevronDoubleLeftIcon
-            class="h-5 w-5 text-gray-400"
-            aria-hidden="true"
-          />
-          0.05s
-        </div>
-      </button>
-    </div>
-  </div>
 
-  <!-- selector -->
-  <div v-show="!useMainState.syncPlayer.subscription.synced.value">
-    <div>
-      <div class="mt-2 flex gap-2 rounded-md">
-        <!-- Youtube -->
-        <div
-          class="flex w-10/12 relative flex-grow items-stretch focus-within:z-10 shadow-sm"
-        >
-          <label
-            for="name"
-            class="absolute -top-2 left-2 inline-block bg-white px-1 text-xs font-medium text-gray-900"
-            >YouTube</label
-          >
-          <!-- Youtube url -->
-          <input
-            type="email"
-            name="email"
-            id="email"
-            class="block w-9/12 rounded-none rounded-l-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-indigo-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            placeholder="https://youtube.com/nLKSSdMWZ8g"
-            v-model="youtubeUrl"
-            @keyup.enter="hundleYoutubeUrlEnter(youtubeUrl)"
-          />
-          <!-- 検索 -->
+<style>
+/** 要素を追加する場合は0px→124pxの間をゆるやかにアニメーションさせる */
+.adjustment-enter-from {
+  height: 0px;
+}
+.adjustment-enter-to {
+  height: 124px;
+}
+/** 要素を削除する場合は124px→0pxの間をゆるやかにアニメーションさせる */
+.adjustment-leave-from {
+  height: 124px;
+}
+.adjustment-leave-to {
+  height: 0px;
+}
+/** from → toのアニメーション設定 */
+.adjustment-enter-active,
+.adjustment-leave-active {
+  transition: all 0.5s ease;
+}
+</style>
+
+<template>
+  <Transition name="adjustment">
+    <div
+      v-show="!useMainState.syncPlayer.subscription.synced.value"
+      class="overflow-hidden"
+    >
+      <!-- adjustment-->
+      <div>
+        <!-- 進む -->
+        <div class="mt-2 flex gap-2 justify-between">
           <button
-            type="button"
-            class="relative w-3/12 -ml-px inline-flex items-center gap-x-1.5 rounded-r-md text-sm font-semibold bg-white text-gray-900 ring-1 ring-inset ring-indigo-300 hover:bg-gray-100"
-            @click="useMainState.youtubeModal.open(playerNo)"
+            class="rounded-md shadow-sm bg-white w-1/4 px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
+            @click="hundleVideoSeek(1)"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="2"
-              width="20px"
-              height="20px"
-              class="stroke-gray-500 mx-auto"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+            <div class="flex items-center justify-center">
+              <ChevronDoubleRightIcon
+                class="h-5 w-5 text-gray-400"
+                aria-hidden="true"
               />
-            </svg>
+              1s
+            </div>
+          </button>
+          <button
+            class="rounded-md shadow-sm bg-white w-1/4 px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
+            @click="hundleVideoSeek(0.5)"
+          >
+            <div class="flex items-center justify-center">
+              <ChevronDoubleRightIcon
+                class="h-5 w-5 text-gray-400"
+                aria-hidden="true"
+              />
+              0.5s
+            </div>
+          </button>
+          <button
+            class="rounded-md shadow-sm bg-white w-1/4 px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
+            @click="hundleVideoSeek(0.1)"
+          >
+            <div class="flex items-center justify-center">
+              <ChevronDoubleRightIcon
+                class="h-5 w-5 text-gray-400"
+                aria-hidden="true"
+              />
+              0.1s
+            </div>
+          </button>
+          <button
+            class="rounded-md shadow-sm bg-white w-1/4 px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
+            @click="hundleVideoSeek(0.05)"
+          >
+            <div class="flex items-center justify-center">
+              <ChevronDoubleRightIcon
+                class="h-5 w-5 text-gray-400"
+                aria-hidden="true"
+              />
+              0.05s
+            </div>
           </button>
         </div>
-        <!-- 端末動画選択 -->
-        <button
-          class="rounded-md shadow-sm bg-white w-2/12 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-100 focus:z-10"
-          @click="hundleLocalVideoSelect()"
-        >
-          <div class="flex items-center justify-center">
-            <svg
-              width="20"
-              height="20"
-              viewBox="0 0 56 41"
-              xmlns="http://www.w3.org/2000/svg"
-              class="fill-gray-500"
-            >
-              <path
-                d="M43.6667 7.33333C43.6667 5.91885 43.1048 4.56229 42.1046 3.5621C41.1044 2.5619 39.7478 2 38.3333 2H6.33333C4.91885 2 3.56229 2.5619 2.5621 3.5621C1.5619 4.56229 1 5.91885 1 7.33333V34C1 35.4145 1.5619 36.771 2.5621 37.7712C3.56229 38.7714 4.91885 39.3333 6.33333 39.3333H38.3333C39.7478 39.3333 41.1044 38.7714 42.1046 37.7712C43.1048 36.771 43.6667 35.4145 43.6667 34V25.112L54.3333 34V7.33333L43.6667 16.2213V7.33333ZM33 23.3333H25V31.3333H19.6667V23.3333H11.6667V18H19.6667V10H25V18H33V23.3333Z"
+        <!-- 戻る -->
+        <div class="mt-2 flex gap-2 justify-between">
+          <button
+            class="rounded-md shadow-sm bg-white w-1/4 px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
+            @click="hundleVideoSeek(-1)"
+          >
+            <div class="flex items-center justify-center">
+              <ChevronDoubleLeftIcon
+                class="h-5 w-5 text-gray-400"
+                aria-hidden="true"
               />
-            </svg>
+              1s
+            </div>
+          </button>
+          <button
+            class="rounded-md shadow-sm bg-white w-1/4 px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
+            @click="hundleVideoSeek(-0.5)"
+          >
+            <div class="flex items-center justify-center">
+              <ChevronDoubleLeftIcon
+                class="h-5 w-5 text-gray-400"
+                aria-hidden="true"
+              />
+              0.5s
+            </div>
+          </button>
+          <button
+            class="rounded-md shadow-sm bg-white w-1/4 px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
+            @click="hundleVideoSeek(-0.1)"
+          >
+            <div class="flex items-center justify-center">
+              <ChevronDoubleLeftIcon
+                class="h-5 w-5 text-gray-400"
+                aria-hidden="true"
+              />
+              0.1s
+            </div>
+          </button>
+          <button
+            class="rounded-md shadow-sm bg-white w-1/4 px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
+            @click="hundleVideoSeek(-0.05)"
+          >
+            <div class="flex items-center justify-center">
+              <ChevronDoubleLeftIcon
+                class="h-5 w-5 text-gray-400"
+                aria-hidden="true"
+              />
+              0.05s
+            </div>
+          </button>
+        </div>
+      </div>
+
+      <!-- selector -->
+      <div>
+        <div>
+          <div class="mt-2 flex gap-2 rounded-md">
+            <!-- Youtube -->
+            <div
+              class="flex w-10/12 relative flex-grow items-stretch focus-within:z-10 shadow-sm"
+            >
+              <label
+                for="name"
+                class="absolute -top-2 left-2 inline-block bg-white px-1 text-xs font-medium text-gray-900"
+                >YouTube</label
+              >
+              <!-- Youtube url -->
+              <input
+                type="email"
+                name="email"
+                id="email"
+                class="block w-9/12 rounded-none rounded-l-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-indigo-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                placeholder="https://youtube.com/nLKSSdMWZ8g"
+                v-model="youtubeUrl"
+                @keyup.enter="hundleYoutubeUrlEnter(youtubeUrl)"
+              />
+              <!-- 検索 -->
+              <button
+                type="button"
+                class="relative w-3/12 -ml-px inline-flex items-center gap-x-1.5 rounded-r-md text-sm font-semibold bg-white text-gray-900 ring-1 ring-inset ring-indigo-300 hover:bg-gray-100"
+                @click="useMainState.youtubeModal.open(playerNo)"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="2"
+                  width="20px"
+                  height="20px"
+                  class="stroke-gray-500 mx-auto"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+                  />
+                </svg>
+              </button>
+            </div>
+            <!-- 端末動画選択 -->
+            <button
+              class="rounded-md shadow-sm bg-white w-2/12 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-100 focus:z-10"
+              @click="hundleLocalVideoSelect()"
+            >
+              <div class="flex items-center justify-center">
+                <svg
+                  width="20"
+                  height="20"
+                  viewBox="0 0 56 41"
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="fill-gray-500"
+                >
+                  <path
+                    d="M43.6667 7.33333C43.6667 5.91885 43.1048 4.56229 42.1046 3.5621C41.1044 2.5619 39.7478 2 38.3333 2H6.33333C4.91885 2 3.56229 2.5619 2.5621 3.5621C1.5619 4.56229 1 5.91885 1 7.33333V34C1 35.4145 1.5619 36.771 2.5621 37.7712C3.56229 38.7714 4.91885 39.3333 6.33333 39.3333H38.3333C39.7478 39.3333 41.1044 38.7714 42.1046 37.7712C43.1048 36.771 43.6667 35.4145 43.6667 34V25.112L54.3333 34V7.33333L43.6667 16.2213V7.33333ZM33 23.3333H25V31.3333H19.6667V23.3333H11.6667V18H19.6667V10H25V18H33V23.3333Z"
+                  />
+                </svg>
+              </div>
+            </button>
           </div>
-        </button>
+        </div>
+        <input
+          type="file"
+          :ref="elements.localVideo.file"
+          @change="hundleLocalVideoChange"
+          hidden
+        />
       </div>
     </div>
-    <input
-      type="file"
-      :ref="elements.localVideo.file"
-      @change="hundleLocalVideoChange"
-      hidden
-    />
-  </div>
+  </Transition>
 
   <!-- Video -->
   <div :ref="elements.videoArea">
