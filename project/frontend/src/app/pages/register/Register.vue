a96d364158a568112627a0b4bbd9b28a95aa1f4b
ritogk <homing0321r4cfw@yahoo.co.jp>
2023-05-16 12:35:43 +0900

    ボタン名を新規登録に修正

--- project/frontend/src/app/pages/register/Register.vue
+++ project/frontend/src/app/pages/register/Register.vue
@@ -246,16 +246,12 @@ const onSubmit = async () => {
           </div>
 
           <div>
-            <div></div>
-          </div>
-
-          <div class="mt-4">
             <button
               type="button"
-              class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 mt-7"
               @click="onSubmit()"
             >
-              ログイン
+              新規登録
             </button>
           </div>
         </div>
