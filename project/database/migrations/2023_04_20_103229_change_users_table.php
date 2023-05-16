0179f6bdfd274d8cf94c667ccf3fe03f11862224
ryoji <ryoji@unko.com>
2023-05-16 18:28:56 +0900

    リセット時にエラーがでないようにした

--- project/database/migrations/2023_04_20_103229_change_users_table.php
+++ project/database/migrations/2023_04_20_103229_change_users_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 class ChangeUsersTable extends Migration
 {
@@ -14,6 +15,7 @@ class ChangeUsersTable extends Migration
     public function up()
     {
         Schema::table('comparisons', function (Blueprint $table) {
+            DB::statement('UPDATE comparisons SET user_id = null WHERE user_id = 0');
             $table->Integer('user_id')->nullable(true)->change();
         });
     }
@@ -26,6 +28,7 @@ class ChangeUsersTable extends Migration
     public function down()
     {
         Schema::table('comparisons', function (Blueprint $table) {
+            DB::statement('UPDATE comparisons SET user_id = 0 WHERE user_id is null');
             $table->Integer('user_id')->nullable(false)->change();
         });
     }
