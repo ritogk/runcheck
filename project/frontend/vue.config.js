6a342b4d97451e849a333a134942e3fd7de99905
ryoji <ryoji@unko.com>
2023-05-18 17:28:53 +0900

    yarn buildで動くようにした

--- project/frontend/vue.config.js
+++ project/frontend/vue.config.js
@@ -1,8 +1,20 @@
 const { defineConfig } = require("@vue/cli-service")
 module.exports = defineConfig({
+  configureWebpack: {
+    output: {
+      filename: "runcheck-[name].js",
+      chunkFilename: "runcheck-[name].js",
+    },
+  },
+  css: {
+    extract: {
+      filename: "runcheck-[name].css",
+      chunkFilename: "runcheck-[name].css",
+    },
+  },
+  productionSourceMap: false,
   outputDir: "../public/spa",
   publicPath: "/spa",
-
   devServer: {
     port: 8080,
     client: {
--- project/resources/views/app.blade.php
+++ project/resources/views/app.blade.php
@@ -5,8 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no">
     <title>RunCheck</title>
-    <script defer src="{{ asset('/spa/js/chunk-vendors.js?v='.time(), true) }}"></script>
-    <script defer src="{{ asset('/spa/js/app.js?v='.time(), true) }}"></script>
+    <script defer src="{{ asset('/spa/runcheck-app.js?v='.time(), true) }}"></script>
+    <script defer src="{{ asset('/spa/runcheck-chunk-vendors.js?v='.time(), true) }}"></script>
+    <link rel="stylesheet" href="{{ asset('/spa/runcheck-app.css?v='.time(), true) }}">
     
   </head>
   <body style="background: rgb(243, 244, 246);">
