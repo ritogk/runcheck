acff1b5fe8d5f48cf7ede6e69b47a5405a87522e
ryoji <ryoji@unko.com>
2023-05-18 19:27:16 +0900

    未使用コード削除

--- project/webpack.mix.js
+++ project/webpack.mix.js
@@ -10,32 +10,3 @@ const mix = require("laravel-mix")
  | file for the application as well as bundling up all the JS files.
  |
  */
-
-mix
-  .js("resources/js/app.js", "public/js")
-  .sass("resources/sass/app.scss", "public/css")
-  .version()
-
-mix.scripts(["resources/js/analytics.js"], "public/js/analytics.js").version()
-
-mix.sass("resources/sass/video.scss", "public/css").version()
-
-mix.scripts(["resources/js/home.js"], "public/js/home.js").version()
-mix
-  .scripts(
-    ["resources/js/youYouControl.js", "resources/js/youYouVideo.js"],
-    "public/js/youYou.js"
-  )
-  .version()
-mix
-  .scripts(
-    ["resources/js/youlocalControl.js", "resources/js/youLocalVideo.js"],
-    "public/js/youLocal.js"
-  )
-  .version()
-mix
-  .scripts(
-    ["resources/js/localLocalControl.js", "resources/js/localLocalVideo.js"],
-    "public/js/localLocal.js"
-  )
-  .version()
