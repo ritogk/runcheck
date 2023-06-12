6ac0a244e1b1859e645c2b472286a9dd19f7d113
ryoji <ryoji@unko.com>
2023-06-12 10:21:00 +0900

    末尾のスラッシュを削除

--- project/resources/views/app.blade.php
+++ project/resources/views/app.blade.php
@@ -5,11 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no">
     <!-- twittter -->
-    <meta name="twitter:card" content="summary" />
-    <meta name="twitter:domain" content="runcheck.homisoftware.net" />
-    <meta name="twitter:title" content="ここをクリックで再生!!" />
-    <meta name="twitter:description" content="車載動画でサーキットを攻略！タイムアップのための比較アプリ" />
-    <meta name="twitter:image" content="{{ asset('/twitter-card.png') }}" />
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:domain" content="runcheck.homisoftware.net">
+    <meta name="twitter:title" content="ここをクリックで再生!!">
+    <meta name="twitter:description" content="車載動画でサーキットを攻略！タイムアップのための比較アプリ">
+    <meta name="twitter:image" content="{{ asset('/twitter-card.png') }}">
 
     <title>RunCheck</title>
     <script defer src="{{ asset('/spa/runcheck-app.js?v='.time(), true) }}"></script>
