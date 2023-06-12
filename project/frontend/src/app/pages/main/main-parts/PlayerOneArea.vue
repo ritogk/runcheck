ede2fa31c48a836e70cbc2d42ed42ee9311ab41b
ryoji <ryoji@unko.com>
2023-06-12 14:51:43 +0900

    調整要素をコンポーネント化

--- project/frontend/src/app/pages/main/main-parts/PlayerOneArea.vue
+++ project/frontend/src/app/pages/main/main-parts/PlayerOneArea.vue
@@ -3,13 +3,12 @@ import { ref, inject, watch } from "vue"
 import { PlayerNo } from "@/app/pages/main/use-main-state-parts/YoutubeSelectorModalState"
 import { YouTubePlayer } from "./player-area-parts/YouTubePlayer"
 import { LocalVideoPlayer } from "./player-area-parts/LocalVideoPlayer"
+import AdjustmentArea from "./player-area-parts/AdjustmentArea.vue"
 import {
   UseMainStateKey,
   UseMainStateType,
 } from "@/app/pages/main/UseMainState"
 import {
-  ChevronDoubleRightIcon,
-  ChevronDoubleLeftIcon,
   VideoCameraIcon,
   // SearchIcon,
 } from "@heroicons/vue/20/solid"
@@ -62,11 +61,6 @@ const hundleYoutubeUrlEnter = async (youtubeUrl: string) => {
   playerOne.value = player
 }
 
-const hundleVideoSeek = async (seconds: number) => {
-  const currentPosition = await playerOne.value.getCurrentTime()
-  useMainState.syncPlayer.playerOne.value.seekTo(currentPosition + seconds)
-}
-
 const hundleYoutubeSearch = () => {
   useMainState.youtubeModal.open(playerNo)
 }
@@ -101,128 +95,7 @@ const hundleYoutubeSearch = () => {
         v-show="!useMainState.syncPlayer.subscription.synced.value"
         class="overflow-hidden"
       >
-        <!-- adjustment-->
-        <div class="px-1">
-          <!-- 進む -->
-          <div class="flex justify-between gap-2">
-            <button
-              class="w-1/4 rounded-md bg-white px-4 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
-              title="1秒進む"
-              aria-label="1秒進む"
-              @click="hundleVideoSeek(1)"
-            >
-              <div class="flex items-center justify-center">
-                <ChevronDoubleRightIcon
-                  class="h-5 w-5 text-gray-400"
-                  aria-hidden="true"
-                />
-                1s
-              </div>
-            </button>
-            <button
-              class="w-1/4 rounded-md bg-white px-4 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
-              title="0.5秒進む"
-              aria-label="0.5秒進む"
-              @click="hundleVideoSeek(0.5)"
-            >
-              <div class="flex items-center justify-center">
-                <ChevronDoubleRightIcon
-                  class="h-5 w-5 text-gray-400"
-                  aria-hidden="true"
-                />
-                0.5s
-              </div>
-            </button>
-            <button
-              class="w-1/4 rounded-md bg-white px-4 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
-              title="0.1秒進む"
-              aria-label="0.1秒進む"
-              @click="hundleVideoSeek(0.1)"
-            >
-              <div class="flex items-center justify-center">
-                <ChevronDoubleRightIcon
-                  class="h-5 w-5 text-gray-400"
-                  aria-hidden="true"
-                />
-                0.1s
-              </div>
-            </button>
-            <button
-              class="w-1/4 rounded-md bg-white px-4 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
-              title="0.05秒進む"
-              aria-label="0.05秒進む"
-              @click="hundleVideoSeek(0.05)"
-            >
-              <div class="flex items-center justify-center">
-                <ChevronDoubleRightIcon
-                  class="h-5 w-5 text-gray-400"
-                  aria-hidden="true"
-                />
-                0.05s
-              </div>
-            </button>
-          </div>
-          <!-- 戻る -->
-          <div class="mt-2 flex justify-between gap-2">
-            <button
-              class="w-1/4 rounded-md bg-white px-4 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
-              title="1秒戻る"
-              aria-label="1秒戻る"
-              @click="hundleVideoSeek(-1)"
-            >
-              <div class="flex items-center justify-center">
-                <ChevronDoubleLeftIcon
-                  class="h-5 w-5 text-gray-400"
-                  aria-hidden="true"
-                />
-                1s
-              </div>
-            </button>
-            <button
-              class="w-1/4 rounded-md bg-white px-4 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
-              title="0.5秒戻る"
-              aria-label="0.5秒戻る"
-              @click="hundleVideoSeek(-0.5)"
-            >
-              <div class="flex items-center justify-center">
-                <ChevronDoubleLeftIcon
-                  class="h-5 w-5 text-gray-400"
-                  aria-hidden="true"
-                />
-                0.5s
-              </div>
-            </button>
-            <button
-              class="w-1/4 rounded-md bg-white px-4 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
-              title="0.1秒戻る"
-              aria-label="0.1秒戻る"
-              @click="hundleVideoSeek(-0.1)"
-            >
-              <div class="flex items-center justify-center">
-                <ChevronDoubleLeftIcon
-                  class="h-5 w-5 text-gray-400"
-                  aria-hidden="true"
-                />
-                0.1s
-              </div>
-            </button>
-            <button
-              class="w-1/4 rounded-md bg-white px-4 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
-              title="0.05秒戻る"
-              aria-label="0.05秒戻る"
-              @click="hundleVideoSeek(-0.05)"
-            >
-              <div class="flex items-center justify-center">
-                <ChevronDoubleLeftIcon
-                  class="h-5 w-5 text-gray-400"
-                  aria-hidden="true"
-                />
-                0.05s
-              </div>
-            </button>
-          </div>
-        </div>
-
+        <AdjustmentArea :player="playerOne" class="px-1"></AdjustmentArea>
         <!-- selector -->
         <div class="px-1">
           <div>
--- project/frontend/src/app/pages/main/main-parts/PlayerTwoArea.vue
+++ project/frontend/src/app/pages/main/main-parts/PlayerTwoArea.vue
@@ -7,12 +7,8 @@ import {
   UseMainStateKey,
   UseMainStateType,
 } from "@/app/pages/main/UseMainState"
-import {
-  ChevronDoubleRightIcon,
-  ChevronDoubleLeftIcon,
-  VideoCameraIcon,
-  // SearchIcon,
-} from "@heroicons/vue/20/solid"
+import { VideoCameraIcon } from "@heroicons/vue/20/solid"
+import AdjustmentArea from "./player-area-parts/AdjustmentArea.vue"
 import { VideoType } from "./player-area-parts/IVideoPlayer"
 import { extractYoutubeId } from "@/core/extractYoutubeId"
 
@@ -60,11 +56,6 @@ const hundleYoutubeUrlEnter = async (youtubeUrl: string) => {
   const player = new YouTubePlayer("youtube-video-two", youtubeId)
   player.load()
   playerTwo.value = player
-}
-
-const hundleVideoSeek = async (seconds: number) => {
-  const currentPosition = await playerTwo.value.getCurrentTime()
-  useMainState.syncPlayer.playerTwo.value.seekTo(currentPosition + seconds)
 }
 
 const hundleYoutubeSearch = () => {
@@ -215,126 +206,7 @@ const hundleYoutubeSearch = () => {
         </div>
 
         <!-- adjustment-->
-        <div class="px-1">
-          <!-- 進む -->
-          <div class="mt-2 flex justify-between gap-2">
-            <button
-              class="w-1/4 rounded-md bg-white px-4 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
-              title="1秒進む"
-              aria-label="1秒進む"
-              @click="hundleVideoSeek(1)"
-            >
-              <div class="flex items-center justify-center">
-                <ChevronDoubleRightIcon
-                  class="h-5 w-5 text-gray-400"
-                  aria-hidden="true"
-                />
-                1s
-              </div>
-            </button>
-            <button
-              class="w-1/4 rounded-md bg-white px-4 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
-              title="0.5秒進む"
-              aria-label="0.5秒進む"
-              @click="hundleVideoSeek(0.5)"
-            >
-              <div class="flex items-center justify-center">
-                <ChevronDoubleRightIcon
-                  class="h-5 w-5 text-gray-400"
-                  aria-hidden="true"
-                />
-                0.5s
-              </div>
-            </button>
-            <button
-              class="w-1/4 rounded-md bg-white px-4 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
-              title="0.1秒進む"
-              aria-label="0.1秒進む"
-              @click="hundleVideoSeek(0.1)"
-            >
-              <div class="flex items-center justify-center">
-                <ChevronDoubleRightIcon
-                  class="h-5 w-5 text-gray-400"
-                  aria-hidden="true"
-                />
-                0.1s
-              </div>
-            </button>
-            <button
-              class="w-1/4 rounded-md bg-white px-4 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
-              title="0.05秒進む"
-              aria-label="0.05秒進む"
-              @click="hundleVideoSeek(0.05)"
-            >
-              <div class="flex items-center justify-center">
-                <ChevronDoubleRightIcon
-                  class="h-5 w-5 text-gray-400"
-                  aria-hidden="true"
-                />
-                0.05s
-              </div>
-            </button>
-          </div>
-          <!-- 戻る -->
-          <div class="mt-2 flex justify-between gap-2">
-            <button
-              class="w-1/4 rounded-md bg-white px-4 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
-              title="1秒戻る"
-              aria-label="1秒戻る"
-              @click="hundleVideoSeek(-1)"
-            >
-              <div class="flex items-center justify-center">
-                <ChevronDoubleLeftIcon
-                  class="h-5 w-5 text-gray-400"
-                  aria-hidden="true"
-                />
-                1s
-              </div>
-            </button>
-            <button
-              class="w-1/4 rounded-md bg-white px-4 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
-              title="0.5秒戻る"
-              aria-label="0.5秒戻る"
-              @click="hundleVideoSeek(-0.5)"
-            >
-              <div class="flex items-center justify-center">
-                <ChevronDoubleLeftIcon
-                  class="h-5 w-5 text-gray-400"
-                  aria-hidden="true"
-                />
-                0.5s
-              </div>
-            </button>
-            <button
-              class="w-1/4 rounded-md bg-white px-4 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
-              title="0.1秒戻る"
-              aria-label="0.1秒戻る"
-              @click="hundleVideoSeek(-0.1)"
-            >
-              <div class="flex items-center justify-center">
-                <ChevronDoubleLeftIcon
-                  class="h-5 w-5 text-gray-400"
-                  aria-hidden="true"
-                />
-                0.1s
-              </div>
-            </button>
-            <button
-              class="w-1/4 rounded-md bg-white px-4 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
-              title="0.05秒戻る"
-              aria-label="0.05秒戻る"
-              @click="hundleVideoSeek(-0.05)"
-            >
-              <div class="flex items-center justify-center">
-                <ChevronDoubleLeftIcon
-                  class="h-5 w-5 text-gray-400"
-                  aria-hidden="true"
-                />
-                0.05s
-              </div>
-            </button>
-          </div>
-        </div>
+        <AdjustmentArea :player="playerTwo" class="mt-2 px-1"></AdjustmentArea>
       </div>
     </Transition>
   </div>
