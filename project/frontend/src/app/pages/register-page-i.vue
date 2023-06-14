33fee939c6cb6f6427b60dd3da7bea056c266ea2
ryoji <ryoji@unko.com>
2023-06-14 15:13:59 +0900

    パスワード用のコンポーネント作成

--- project/frontend/src/app/pages/register-page-i.vue
+++ project/frontend/src/app/pages/register-page-i.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
-import { ref, inject, watch } from "vue"
+import { ref, Ref, inject, watch } from "vue"
 import { UseAlretStateKey, UseAlretStateType } from "@/app/alret-state"
 import { useRouter } from "vue-router"
 import { UseUserStateKey, UseUserStateType } from "@/app/user-state"
 import { UseLoadingStateKey, UseLoadingStateType } from "@/app/loading-state"
 import FormInputText from "@/components/form-input-text.vue"
+import InputPassword from "@/components/input-password.vue"
 
 const form = {
   hanndleName: {
@@ -32,14 +33,14 @@ const form = {
     id: "password",
     required: true,
     label: "パスワード",
-    placeholder: "",
+    placeholder: "パスワード",
     value: ref<string>(""),
   },
   passwordConfirm: {
     id: "password-confirm",
     required: true,
     label: "パスワード(確認)",
-    placeholder: "",
+    placeholder: "確認",
     value: ref<string>(""),
   },
 }
@@ -49,33 +50,15 @@ const useAlertState = inject(UseAlretStateKey) as UseAlretStateType
 const useLoadingState = inject(UseLoadingStateKey) as UseLoadingStateType
 const userState = inject(UseUserStateKey) as UseUserStateType
 
-const onSwitchPasswordVisibility = (type: "password" | "password-confirm") => {
-  switch (type) {
-    case "password":
-      const passwordElement = <HTMLInputElement>(
-        document.getElementById(form.password.id)
-      )
-      passwordElement.type === "password" ? "text" : "password"
-      break
-    case "password-confirm":
-      const passwordConfirmElement = <HTMLInputElement>(
-        document.getElementById(form.passwordConfirm.id)
-      )
-      passwordConfirmElement.type === "password" ? "text" : "password"
-  }
-}
-
 const onSubmit = async () => {
-  const passwordElement = <HTMLInputElement>(
-    document.getElementById(form.password.id)
-  )
   const passwordConfirmElement = <HTMLInputElement>(
     document.getElementById(form.passwordConfirm.id)
   )
   // 独自エラーの初期化
   passwordConfirmElement.setCustomValidity("")
+  debugger
   // パスワードチェック
-  if (passwordElement.value !== passwordConfirmElement.value) {
+  if (form.password.value.value !== form.passwordConfirm.value.value) {
     passwordConfirmElement.setCustomValidity("パスワードが一致しません。")
     passwordConfirmElement.reportValidity()
     return
@@ -86,7 +69,7 @@ const onSubmit = async () => {
       form.hanndleName.value.value,
       form.carType.value.value,
       form.email.value.value,
-      passwordElement.value
+      form.password.value.value
     )
     useLoadingState.stop(loadingId)
     useAlertState.clear()
@@ -98,10 +81,6 @@ const onSubmit = async () => {
   }
   useLoadingState.stop(loadingId)
 }
-
-watch(form.hanndleName.value, (value) => {
-  console.log(value)
-})
 </script>
 
 <template>
@@ -116,117 +95,73 @@ watch(form.hanndleName.value, (value) => {
 
     <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
       <div class="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
-        <form @submit.prevent="onSubmit">
-          <div class="space-y-4">
-            <FormInputText
-              :id="form.hanndleName.id"
-              :required="form.hanndleName.required"
-              :label="form.hanndleName.label"
-              :placeholder="form.hanndleName.placeholder"
-              @input="form.hanndleName.value.value = $event"
-            ></FormInputText>
+        <div class="space-y-4">
+          <FormInputText
+            :id="form.hanndleName.id"
+            :value="form.hanndleName.value.value"
+            :required="form.hanndleName.required"
+            :label="form.hanndleName.label"
+            :placeholder="form.hanndleName.placeholder"
+            @input="form.hanndleName.value.value = $event"
+          ></FormInputText>
 
-            <FormInputText
-              :id="form.carType.id"
-              :required="form.carType.required"
-              :label="form.carType.label"
-              :placeholder="form.carType.placeholder"
-              @input="form.carType.value.value = $event"
-            ></FormInputText>
+          <FormInputText
+            :id="form.carType.id"
+            :value="form.carType.value.value"
+            :required="form.carType.required"
+            :label="form.carType.label"
+            :placeholder="form.carType.placeholder"
+            @input="form.carType.value.value = $event"
+          ></FormInputText>
 
-            <FormInputText
-              :id="form.email.id"
-              :required="form.email.required"
-              :label="form.email.label"
-              :placeholder="form.email.placeholder"
-              @input="form.email.value.value = $event"
-            ></FormInputText>
+          <FormInputText
+            :id="form.email.id"
+            :value="form.email.value.value"
+            :required="form.email.required"
+            :label="form.email.label"
+            :placeholder="form.email.placeholder"
+            @input="form.email.value.value = $event"
+          ></FormInputText>
 
-            <div>
-              <label
-                for="password"
-                class="block text-sm font-medium leading-6 text-gray-900"
-                >パスワード<span
-                  class="text-lg leading-none text-red-400"
-                  title="このフィールドは必須です"
-                  >*</span
-                ></label
-              >
-              <div class="mt-2">
-                <div class="flex gap-2">
-                  <div class="relative mt-2 w-1/2 rounded-md shadow-sm">
-                    <input
-                      id="password"
-                      name="password"
-                      type="password"
-                      autocomplete="new-password"
-                      :ref="form.password"
-                      required
-                      pattern="^(?=.*\d)(?=.*[a-zA-Z]).{8,}$"
-                      title="半角英字と半角数字を組み合わせて8文字以上で入力してください。"
-                      class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-slate-600 sm:text-sm sm:leading-6"
-                      placeholder="パスワード"
-                    />
-                    <div
-                      class="absolute inset-y-0 right-0 flex items-center pr-3"
-                    >
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        class="fill-gray-400 hover:fill-gray-300"
-                        width="20"
-                        height="20"
-                        viewBox="0 0 200 145.522"
-                        @click="onSwitchPasswordVisibility('password')"
-                      >
-                        <path
-                          d="M99.363,0C52.631,0,14.243,30.17,0,72.083,14.243,114,52.631,145.522,99.363,145.522S185.754,114,200,72.083A106.312,106.312,0,0,0,99.363,0ZM97.382,24.478a47.647,47.647,0,1,1,1.981,95.254,47.643,47.643,0,1,1-1.981-95.265ZM99.363,39.1A32.987,32.987,0,1,0,132.35,72.083,32.987,32.987,0,0,0,99.363,39.1Zm8.233,8.743a11.454,11.454,0,1,1-11.215,11.69A11.455,11.455,0,0,1,107.6,47.839Z"
-                        />
-                      </svg>
-                    </div>
-                  </div>
-                  <div class="relative mt-2 w-1/2 rounded-md shadow-sm">
-                    <input
-                      id="password-confirm"
-                      name="password-confirm"
-                      type="password"
-                      autocomplete="password-confirm"
-                      :ref="form.passwordConfirm"
-                      required
-                      pattern="^(?=.*\d)(?=.*[a-zA-Z]).{8,}$"
-                      title="半角英字と半角数字を組み合わせて8文字以上で入力してください。"
-                      class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-slate-600 sm:text-sm sm:leading-6"
-                      placeholder="確認"
-                    />
-                    <div
-                      class="absolute inset-y-0 right-0 flex items-center pr-3"
-                    >
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        class="fill-gray-400 hover:fill-gray-300"
-                        width="20"
-                        height="20"
-                        viewBox="0 0 200 145.522"
-                        @click="onSwitchPasswordVisibility('password-confirm')"
-                      >
-                        <path
-                          d="M99.363,0C52.631,0,14.243,30.17,0,72.083,14.243,114,52.631,145.522,99.363,145.522S185.754,114,200,72.083A106.312,106.312,0,0,0,99.363,0ZM97.382,24.478a47.647,47.647,0,1,1,1.981,95.254,47.643,47.643,0,1,1-1.981-95.265ZM99.363,39.1A32.987,32.987,0,1,0,132.35,72.083,32.987,32.987,0,0,0,99.363,39.1Zm8.233,8.743a11.454,11.454,0,1,1-11.215,11.69A11.455,11.455,0,0,1,107.6,47.839Z"
-                        />
-                      </svg>
-                    </div>
-                  </div>
-                </div>
+          <div>
+            <label
+              for="password"
+              class="block text-sm font-medium leading-6 text-gray-900"
+              >パスワード<span
+                class="text-lg leading-none text-red-400"
+                title="このフィールドは必須です"
+                >*</span
+              ></label
+            >
+            <div class="mt-2">
+              <div class="flex gap-2">
+                <InputPassword
+                  class="w-1/2"
+                  :id="form.password.id"
+                  :placeholder="form.password.placeholder"
+                  @input="form.password.value.value = $event"
+                ></InputPassword>
+
+                <InputPassword
+                  class="w-1/2"
+                  :id="form.passwordConfirm.id"
+                  :placeholder="form.passwordConfirm.placeholder"
+                  @input="form.passwordConfirm.value.value = $event"
+                ></InputPassword>
               </div>
             </div>
-
-            <div>
-              <button
-                class="mt-7 flex w-full justify-center rounded-md bg-slate-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-slate-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-slate-600"
-              >
-                新規登録
-              </button>
-            </div>
           </div>
-        </form>
+
+          <div>
+            <button
+              type="button"
+              @click="onSubmit"
+              class="mt-7 flex w-full justify-center rounded-md bg-slate-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-slate-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-slate-600"
+            >
+              新規登録
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   </div>
---
+++ project/frontend/src/components/input-password.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+const props = defineProps<{
+  value: string
+  id: string
+  placeholder: string
+}>()
+
+const emits = defineEmits<{ (e: "input", value: string): void }>()
+
+const hundleInput = (event: any) => {
+  emits("input", event.target.value)
+}
+
+const switchPasswordVisibility = () => {
+  const element = <HTMLInputElement>document.getElementById(props.id)
+  element.type = element.type === "password" ? "text" : "password"
+}
+</script>
+
+<template>
+  <div class="relative mt-2 rounded-md shadow-sm">
+    <input
+      :id="props.id"
+      :name="props.id"
+      :placeholder="props.placeholder"
+      :value="props.value"
+      required
+      type="password"
+      autocomplete="new-password"
+      @input="hundleInput"
+      pattern="^(?=.*\d)(?=.*[a-zA-Z]).{8,}$"
+      title="半角英字と半角数字を組み合わせて8文字以上で入力してください。"
+      class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-slate-600 sm:text-sm sm:leading-6"
+    />
+    <div class="absolute inset-y-0 right-0 flex items-center pr-3">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="fill-gray-400 hover:fill-gray-300"
+        width="20"
+        height="20"
+        viewBox="0 0 200 145.522"
+        @click="switchPasswordVisibility"
+      >
+        <path
+          d="M99.363,0C52.631,0,14.243,30.17,0,72.083,14.243,114,52.631,145.522,99.363,145.522S185.754,114,200,72.083A106.312,106.312,0,0,0,99.363,0ZM97.382,24.478a47.647,47.647,0,1,1,1.981,95.254,47.643,47.643,0,1,1-1.981-95.265ZM99.363,39.1A32.987,32.987,0,1,0,132.35,72.083,32.987,32.987,0,0,0,99.363,39.1Zm8.233,8.743a11.454,11.454,0,1,1-11.215,11.69A11.455,11.455,0,0,1,107.6,47.839Z"
+        />
+      </svg>
+    </div>
+  </div>
+</template>
