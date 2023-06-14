d7d8f2790dbf3005a8ae977e1ab0ddf6408a8fe7
ryoji <ryoji@unko.com>
2023-06-14 15:52:55 +0900

    メールドアドレス用のコンポーネント作成

---
+++ project/frontend/src/components/form-input-email.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import InputEmail from "./input-email.vue"
+
+const props = defineProps<{
+  value: string
+  id: string
+  placeholder: string
+  label: string
+  required: boolean
+}>()
+
+const emits = defineEmits<{ (e: "input", value: string): void }>()
+
+const hundleInput = (value: string) => {
+  emits("input", value)
+}
+</script>
+
+<template>
+  <div>
+    <label
+      :for="props.id"
+      class="block text-sm font-medium leading-6 text-gray-900"
+      :required="props.required"
+      >{{ props.label
+      }}<span
+        v-if="props.required"
+        class="text-lg leading-none text-red-400"
+        title="このフィールドは必須です"
+        >*</span
+      ></label
+    >
+    <div class="mt-2">
+      <InputEmail
+        :id="props.id"
+        :value="props.value"
+        :required="props.required"
+        :label="props.label"
+        :placeholder="props.placeholder"
+        @input="hundleInput"
+      ></InputEmail>
+    </div>
+  </div>
+</template>
