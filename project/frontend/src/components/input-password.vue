ef11857457483cef565f310c93195ece5ce99aef
ryoji <ryoji@unko.com>
2023-06-15 09:28:26 +0900

    表示切替をキーボード操作から行えるようにした

--- project/frontend/src/components/input-password.vue
+++ project/frontend/src/components/input-password.vue
@@ -40,19 +40,22 @@ const switchPasswordVisibility = () => {
       @change="hundleChange"
       class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-slate-600 sm:text-sm sm:leading-6"
     />
-    <div class="absolute inset-y-0 right-0 flex items-center pr-3">
+    <button
+      type="button"
+      @click="switchPasswordVisibility"
+      class="absolute inset-y-0 right-0 flex items-center px-2"
+    >
       <svg
         xmlns="http://www.w3.org/2000/svg"
         class="fill-gray-400 hover:fill-gray-300"
         width="20"
         height="20"
         viewBox="0 0 200 145.522"
-        @click="switchPasswordVisibility"
       >
         <path
           d="M99.363,0C52.631,0,14.243,30.17,0,72.083,14.243,114,52.631,145.522,99.363,145.522S185.754,114,200,72.083A106.312,106.312,0,0,0,99.363,0ZM97.382,24.478a47.647,47.647,0,1,1,1.981,95.254,47.643,47.643,0,1,1-1.981-95.265ZM99.363,39.1A32.987,32.987,0,1,0,132.35,72.083,32.987,32.987,0,0,0,99.363,39.1Zm8.233,8.743a11.454,11.454,0,1,1-11.215,11.69A11.455,11.455,0,0,1,107.6,47.839Z"
         />
       </svg>
-    </div>
+    </button>
   </div>
 </template>
