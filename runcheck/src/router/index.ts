713d3e0f41cbdd1853656d96f978efc5278cf801
ryoji <ryoji@unko.com>
2023-06-22 18:55:38 +0900

    ルーティング追加

--- runcheck/src/router/index.ts
+++ runcheck/src/router/index.ts
@@ -1,13 +1,36 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import MainPagePI from '@/app/pages/main-page-pi.vue'
+import { createRouter, createWebHistory } from "vue-router"
+import MainPagePI from "@/app/pages/main-page-pi.vue"
+import RegisterPageI from "@/app/pages/register-page-i.vue"
+import LoginPageI from "@/app/pages/login-page-i.vue"
+import HomePageI from "@/app/pages/home-page-i.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/main',
-      name: 'main',
+      path: "/index",
+      name: "index",
       component: MainPagePI
+    },
+    {
+      path: "/main",
+      name: "main",
+      component: MainPagePI
+    },
+    {
+      path: "/register",
+      name: "register",
+      component: RegisterPageI
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: LoginPageI
+    },
+    {
+      path: "/home",
+      name: "home",
+      component: HomePageI
     }
   ]
 })
