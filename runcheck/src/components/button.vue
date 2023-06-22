3209401dec3bf769fcc510835cb99b37f1015fda
ryoji <ryoji@unko.com>
2023-06-22 12:54:08 +0900

    サイズ指定

--- runcheck/src/components/button.vue
+++ runcheck/src/components/button.vue
@@ -3,6 +3,7 @@ const props = defineProps<{
   label?: string
   variant?: "primary" | "danger" | "secondary" | "custom"
   type?: "submit" | "button" | "reset"
+  size?: "sm" | "md" | "lg"
   accessibilityTitle?: string
 }>()
 </script>
@@ -11,14 +12,19 @@ const props = defineProps<{
   <button
     :type="props.type ?? 'button'"
     :class="{
+      // variant
       'bg-slate-600 text-white hover:bg-slate-500 focus-visible:outline-slate-700':
         props.variant === 'primary',
       'bg-red-500 text-white hover:bg-red-400 focus-visible:outline-red-600':
         props.variant === 'danger',
       'bg-white text-gray-800 ring-gray-300 hover:bg-gray-50 focus-visible:outline-gray-400':
-        props.variant === 'secondary' || props.variant === undefined
+        props.variant === 'secondary' || props.variant === undefined,
+      // size
+      'py-1.5': props.size === 'sm',
+      'py-2': props.size === 'md' || props.size === undefined,
+      'py-2.5': props.size === 'lg'
     }"
-    class="rounded-md px-4 py-2 text-sm font-semibold shadow-sm ring-1 ring-inset focus:z-10"
+    class="rounded-md px-4 text-sm font-semibold shadow-sm ring-1 ring-inset focus:z-10"
     :title="props.accessibilityTitle"
     :aria-label="props.accessibilityTitle"
   >
