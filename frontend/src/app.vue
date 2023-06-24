945b367aeaff7e05e0f095147cfb6100d2f4aa56
ryoji <ryoji@unko.com>
2023-06-24 15:50:22 +0900

    不要コンポーネント削除

--- frontend/src/app.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>
-  <div class="flex bg-slate-500 h-screen items-center justify-center">
-    <p class="font-bold text-whopper text-red-400">Hello Tailwind</p>
-  </div>
-</template>
