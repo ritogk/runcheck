d4c55ad4392eeef3cb1231a04052f423266f3c6a
ryoji <ryoji@unko.com>
2023-06-25 11:13:08 +0900

    未使用importをlintから除外

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,14 +1,21 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution');
+require("@rushstack/eslint-patch/modern-module-resolution")
 module.exports = {
   root: true,
-  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/eslint-config-typescript', '@vue/eslint-config-prettier/skip-formatting', 'plugin:storybook/recommended'],
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/eslint-config-typescript",
+    "@vue/eslint-config-prettier/skip-formatting",
+    "plugin:storybook/recommended"
+  ],
   parserOptions: {
-    ecmaVersion: 'latest'
+    ecmaVersion: "latest"
   },
   rules: {
-    'vue/multi-word-component-names': 'off',
-    'vue/no-multiple-template-root': 'off',
-    'no-unused-vars': 'off'
+    "vue/multi-word-component-names": "off",
+    "vue/no-multiple-template-root": "off",
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": "off"
   }
-};
+}
