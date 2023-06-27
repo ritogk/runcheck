7f59ccd72ac32b9cfc962f7c34d34e962040a52a
ryoji <ryoji@unko.com>
2023-06-27 12:51:05 +0900

    refactor

--- frontend/src/core/api-state/use-get-api-youtube-video.ts
+++ frontend/src/core/api-state/use-get-api-youtube-video.ts
@@ -1,6 +1,6 @@
 import { YoutubeApi } from "@/core/openapiClient"
 import { apiConfig } from "@/core/openapi"
-import { useQuery, useQueryClient } from "@tanstack/vue-query"
+import { useQuery } from "@tanstack/vue-query"
 import { GET_YOUTUBE_VIDEO } from "./query-key"
 
 const UseApiGetYoutubeVideo = (isLogined: boolean) => {
@@ -8,14 +8,9 @@ const UseApiGetYoutubeVideo = (isLogined: boolean) => {
   return useQuery({
     queryKey: [GET_YOUTUBE_VIDEO],
     queryFn: () => youtubeApi.youtubeVideosGet(),
+    retry: false,
     enabled: isLogined,
-    staleTime: Infinity, // キャッシュ時間無制限 ※一旦画面更新するまでキャッシュを有効化させる
-    retry: false
+    staleTime: Infinity
   })
 }
 export default UseApiGetYoutubeVideo
-
-// export const refreshCache = () => {
-//   const queryClient = useQueryClient() // こいつはsetup内でしか呼び出せないらしい。って事は呼び出し元
-//   queryClient.invalidateQueries({ queryKey: [GET_YOUTUBE_VIDEO] })
-// }
