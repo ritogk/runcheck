cf075241b68de52d51085e97b893c9b630757751
ryoji <ryoji@unko.com>
2023-09-24 20:00:39 +0900

    スローするようにした

--- backend/app/UseCase/Comparison/PublishComparisonAction.php
+++ backend/app/UseCase/Comparison/PublishComparisonAction.php
@@ -2,6 +2,7 @@
 
 namespace App\UseCase\Comparison;
 
+use \Illuminate\Auth\Access\AuthorizationException;
 use App\Model\Comparison;
 // usecase
 use App\UseCase\Authentication\GetMeAction;
@@ -27,6 +28,8 @@ class PublishComparisonAction
     if (($user && $user->id == $comparison->user_id) || $comparison->anonymous) {
       $comparison->release_kbn = true;
       $comparison->save();
+      return;
     }
+    throw new AuthorizationException();
   }
 }
