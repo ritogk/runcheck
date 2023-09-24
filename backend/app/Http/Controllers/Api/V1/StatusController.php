e2fac4b1a42ec77d749e19865488bf0a3f046e08
ryoji <ryoji@unko.com>
2023-09-24 14:30:56 +0900

    refactor

--- backend/app/Http/Controllers/Api/V1/StatusController.php
+++ backend/app/Http/Controllers/Api/V1/StatusController.php
@@ -6,23 +6,25 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Response;
 //usecase
-use App\UseCase\GetStatusAction;
+use App\UseCase\GetClientStatusAction;
 
 class StatusController extends Controller
 {
     /**
-     * 未ログインユーザーも含めたユーザーの状態を取得
+     * クライアントの状態を取得
      *
      * @return JsonResponse
      */
-    public function status(GetStatusAction $action): JsonResponse
+    public function status(GetClientStatusAction $action): JsonResponse
     {
-        $result = $action->status();
+        $result = $action->get_status();
         $user = $result['user'];
         return response()->json(
-            ['isLogined' => $result['is_logined'],
-            'isYoutubeAuthroized' => $result['is_youtube_authroized'],
-            'user' => ['id' => $user ? $user['id'] : 0, 'name' => $user ? $user['name'] : ""]],
+            [
+                'isLogined' => $result['is_logined'],
+                'isYoutubeAuthroized' => $result['is_youtube_authroized'],
+                'user' => ['id' => $user ? $user['id'] : 0, 'name' => $user ? $user['name'] : ""]
+            ],
             Response::HTTP_OK
         );
     }
---
+++ backend/app/UseCase/GetClientStatusAction.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\UseCase;
+
+// core
+use App\Core\SessionKey;
+// usecase
+use App\UseCase\Authentication\GetMeAction;
+
+class GetClientStatusAction
+{
+  private GetMeAction $me_action;
+  public function __construct(GetMeAction $action)
+  {
+    $this->me_action = $action;
+  }
+
+  public function get_status(): array
+  {
+    $user = $this->me_action->me();
+    if ($user != null) {
+      return $this->get_user_status();
+    } else {
+      return $this->get_anonymous_status();
+    }
+  }
+
+  /**
+   * ユーザーのステータスを取得
+   *
+   * @return array
+   */
+  private function get_user_status(): array
+  {
+    $user = $this->me_action->me();
+    $isYoutubeAuthroized = !empty($user->youtube_token);
+    return ['is_logined' => true, 'is_youtube_authroized' => $isYoutubeAuthroized, 'user' => $user];
+  }
+
+  /**
+   * 匿名ユーザーのステータスを取得
+   *
+   * @return array
+   */
+  private function get_anonymous_status(): array
+  {
+    $accessToken = session()->get(SessionKey::$YOUTUBE_ACCESS_TOKEN);
+    $isYoutubeAuthroized = !empty($accessToken);
+    return ['is_logined' => false, 'is_youtube_authroized' => $isYoutubeAuthroized, 'user' => null];
+  }
+}
